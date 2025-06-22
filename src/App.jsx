d195a3494e83e7192d3d8a5f77973347aa249bbe
ubaydullayevadiyora <146558675+ubaydullayevadiyora@users.noreply.github.com>
2025-06-22 11:11:10 +0500

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -3,51 +3,51 @@ import Counter from "./components/Counter";
 import Players from "./components/Player";
 import Card from "./components/Card";
 const App = () => {
-  // const [products, setProducts] = useState([
-  //   {
-  //     id: 1,
-  //     name: "product-1",
-  //     price: 1200,
-  //     sale: 13,
-  //     quantity: 30,
-  //     img: "./src/assets/react.svg",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "product-1",
-  //     price: 1200,
-  //     sale: 13,
-  //     quantity: 30,
-  //     img: "./src/assets/react.svg",
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "product-1",
-  //     price: 1200,
-  //     sale: 13,
-  //     quantity: 30,
-  //     img: "./src/assets/react.svg",
-  //   },
-  //   {
-  //     id: 4,
-  //     name: "product-1",
-  //     price: 1200,
-  //     sale: 13,
-  //     quantity: 30,
-  //     img: "./src/assets/react.svg",
-  //   },
-  // ]);
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      name: "product-1",
+      price: 1200,
+      sale: 13,
+      quantity: 30,
+      img: "./src/assets/react.svg",
+    },
+    {
+      id: 2,
+      name: "product-1",
+      price: 1200,
+      sale: 13,
+      quantity: 30,
+      img: "./src/assets/react.svg",
+    },
+    {
+      id: 3,
+      name: "product-1",
+      price: 1200,
+      sale: 13,
+      quantity: 30,
+      img: "./src/assets/react.svg",
+    },
+    {
+      id: 4,
+      name: "product-1",
+      price: 1200,
+      sale: 13,
+      quantity: 30,
+      img: "./src/assets/react.svg",
+    },
+  ]);
   return (
     <div className="container">
-  {/* //     <div className="row mt-3">
-  //       {products.map((item) => (
-  //         <div className="col-md-4" key={item.id}>
-  //           <Card item={item} />
-  //         </div>
-  //       ))}
-  //     </div> */}
+      <div className="row mt-3">
+        {products.map((item) => (
+          <div className="col-md-4" key={item.id}>
+            <Card item={item} />
+          </div>
+        ))}
+      </div>
       {/* <Counter /> */}
-      <Players />
+      {/* <Players /> */}
     </div>
   );
 };
